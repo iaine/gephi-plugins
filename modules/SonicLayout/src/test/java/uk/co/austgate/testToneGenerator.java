0941397b04decfb28ef3fabb92d94440d0dae256
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2023-12-07 16:30:10 +0000

    Added initial test file

---
+++ modules/SonicLayout/src/test/java/uk/co/austgate/testToneGenerator.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uk.co.austgate;
+
+import uk.co.austgate.ToneGenerator;
+/**
+ *
+ * @author iain
+ */
+public class testToneGenerator {
+    
+}
