65bcd3933f4029a2bff239b39ed176533e3aee1f
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2023-12-07 16:51:56 +0000

    Added simple chuck command.

--- modules/SonicLayout/src/main/java/uk/co/austgate/PlayChuck.java
+++ modules/SonicLayout/src/main/java/uk/co/austgate/PlayChuck.java
@@ -4,10 +4,52 @@
  */
 package uk.co.austgate;
 
+import java.io.IOException;
+
 /**
  *
  * @author iain
  */
 public class PlayChuck {
-    public void playSound () {}
+    
+    private static String filePath;
+    
+    protected void PlayChuck(String baseDir) {
+        filePath = baseDir;
+        
+        startChuck();
+    }
+
+    /**
+     * Call a Chuck sound from command line
+     * @param chuckCommands 
+     */
+    public void playSound (String[] chuckCommands) {
+      try {
+       
+        Runtime.getRuntime().exec(chuckCommands);
+      } catch (IOException ioe) {
+            System.out.println("IOE " + ioe);
+      }
+    }
+
+    
+    /**
+     * Function to start a Chuck server. 
+     * 
+     * We assume that it is in the ChucK directory. 
+     * @param baseDir 
+     */
+    private void startChuck() {
+      try {
+        String[] command =
+        {
+            "cmd",
+        };
+        
+        Runtime.getRuntime().exec(command);
+      } catch (IOException ioe) {
+            System.out.println("IOE " + ioe);
+      }
+    }
 }
